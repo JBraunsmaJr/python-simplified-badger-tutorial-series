069211f917019403ed71586d81463595b2c9597f
JBraunsmaJr <jonbraunsma@gmail.com>
2022-03-10 23:24:30 -0500

    fixed something

--- March-12-2022_UserInputMadness_Config/starter-content/main.py
+++ March-12-2022_UserInputMadness_Config/starter-content/main.py
@@ -1,10 +1,2 @@
-with open("../../March-06-2022_ReadWriteFiles/starter-content/example.txt") as example_file:
-    line_number = 0
-    while True:
-        current_line = example_file.readline()
-
-        if not current_line:
-            break
-
-        print(f"[{line_number}]\t{current_line.strip()}")
-        line_number += 1
+import json
+from util import get_user_input
